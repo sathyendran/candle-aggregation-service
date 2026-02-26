321aa0d4dd4a2b900402df60e1c71b6367600332
Sathyendran <sathyendran.a@gmail.com>
2026-02-26 19:51:20 +0530

    Initial code setup for candle aggregation service

---
+++ src/main/java/com/multibankfx/AppLauncher.java
@@ -0,0 +1,13 @@
+package com.multibankfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLauncher.class, args);
+	}
+
+}
---
+++ src/test/java/com/multibankfx/AppLauncherTest.java
@@ -0,0 +1,13 @@
+package com.multibankfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLauncherTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
